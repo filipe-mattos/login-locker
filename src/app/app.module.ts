4a6103b0b6b1b5b62746910fde32ed715ce5d5c4
filipemattos <filipe14mattos@gmail.com>
2023-07-09 23:58:26 -0300

    feito estrutura inicial do header e do footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {HomePageComponent} from "./homePage/homePage.component";
 import {PlataformCardComponent} from "./Shared/plataformCard/plataformCard.component";
+import {HeaderComponent} from "./Shared/header/header.component";
+import {FooterComponent} from "./Shared/footer/footer.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     PlataformCardComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
