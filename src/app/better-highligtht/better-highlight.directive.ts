a2a0d528643ad5e8607a919b3a6cc7e3e57a4b26
Filipe Magioni Mattos <filipe.mattos@conexos.inf.br>
2024-01-19 16:36:49 -0300

    Manipulando diretivas com o Render2

--- src/app/better-highligtht/better-highlight.directive.ts
+++ src/app/better-highligtht/better-highlight.directive.ts
@@ -1,16 +1,29 @@
 //Criando uma diretiva
 
 
-import {Directive, ElementRef, HostListener, OnInit, Renderer2} from "@angular/core";
+import {Directive, ElementRef, HostListener, Input, OnInit, Renderer2} from "@angular/core";
 
 @Directive({
   selector: '[appBetterHighlight]',
 })
 
 export class BetterHighlightDirective  implements OnInit{
+  @Input() textColor:  string = 'black';
+  @Input()  backgroundColor:  string  =  'white';
+
   //Aplica um evento direto no elemento pelo DOM
   @HostListener('mouseenter') mouseover(){
+    this.renderer.setStyle(
+      this.elRef.nativeElement,
+      'background-color',
+      'red');
+  }
 
+  @HostListener('mouseleave') mouseleave(){
+    this.renderer.setStyle(
+      this.elRef.nativeElement,
+      'background-color',
+      'blue');
   }
   //Passando para o construtor os atributos que irei utilizar  quando  uma instacia
   //dessa diretiva for criada, utilizando  render para manipular o DOM pois  e uma
